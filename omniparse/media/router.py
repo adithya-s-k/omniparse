4db6c81bf0b69d9d897f5d08c74cfdcf7add2d08
Adithya S K <adithyaskolavi@gmail.com>
2024-06-25 21:00:58 +0530

    media router added - Adithya S K

---
+++ omniparse/media/router.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException , APIRouter,  status , Form
+from fastapi.responses import JSONResponse
+
+from omniparse.media import parse_audio , parse_video
+from omniparse import get_shared_state
+
+media_router = APIRouter()
+model_state = get_shared_state()
+
+@media_router.post("/audio")
+async def parse_audio_endpoint(file: UploadFile = File(...)):
+    try:
+        file_bytes = await file.read()
+        result = parse_audio(file_bytes , model_state)
+        return JSONResponse(content=result)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@media_router.post("/video")
+async def parse_video_endpoint(file: UploadFile = File(...)):
+    try:
+        file_bytes = await file.read()
+        result = parse_video(file_bytes , model_state)
+        return JSONResponse(content=result)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
